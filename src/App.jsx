a9c6f12baa78ca5c20633c88295d2117229745fe
iredith <iredith0112@gmail.com>
2023-10-01 09:37:41 +0530

    [Initial-setup]: created initial setup for ludo game

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function App() {
+  return (
+    <div className="App">
+      <h1>Let's Play LUDO</h1>
+      <p>What do you say?</p>
+    </div>
+  );
+}
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "docs"),
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts"],
+    alias: {
+      "@": path.resolve(__dirname, "src/")
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.?(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "src", "index.html"),
+    }),
+  ],
+};
